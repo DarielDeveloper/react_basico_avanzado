8de4fc6fc7b1588a85bd92dd45dc3f26246f6416
darielna <darielna910806@gmail.com>
2025-08-21 09:48:46 -0400

    mejorando componente button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -3,14 +3,15 @@
 //Se hace con interface porque es mas limpio de leer
 interface Props {
   label: string;
+  title?: string;
   className?: string; //? para decir que no es un campo obligatorio
   parenMethod: () => void;
 }
-export const Button = ({ label, className, parenMethod }: Props) => {
+export const Button = ({ label, title, className, parenMethod }: Props) => {
   //Las llamadas de los métodos se debe ser sin () ya que de esa forma no llamas la referencia de la función creada
   //En este ejemplo parenMethod hace llamado de su referencia, si lo hace parenMethod() estas creando una nueva referencia por lo que el uso de la memoria se excede
   return (
-    <button className={className} onClick={parenMethod}>
+    <button className={className} title={title} onClick={parenMethod}>
       {label}
     </button>
   );
